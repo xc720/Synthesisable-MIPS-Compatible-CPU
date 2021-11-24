e4d54a5fd5caef55a8d59782dea303b508587365
Dominic Justice Konec <dom.justicekonec@gmail.com>
2021-11-24 23:30:09 +0000

    fixing initail state

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -18,13 +18,14 @@ module mips_cpu_bus (
   logic [2:0] state;  //0-halt 1-fetch 2-decode 3-exec1 4-exec2
 
   initial begin
-    state  = 1;
-    active = 1;
+    state  = 0;
+    active = 0;
   end
 
   always_ff @(posedge clk) begin  //state machine
     if (reset) begin
-      state <= 1;
+      state  <= 1;
+      active <= 1;
     end else if (pc_out == 0 && state != 0) begin
       state  <= 0;
       active <= 0;
