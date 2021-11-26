04418fb6bbd0b19034095f11ac4824162f93cf86
Dominic Justice Konec <dom.justicekonec@gmail.com>
2021-11-26 15:25:20 +0000

    format and remove dummies

--- rtl/mips_cpu_memory.v
+++
@@ -1,9 +0,0 @@
-module memory(
-    input logic[31:0] address,
-    input logic[31:0] data,
-    output logic[31:0] mem_data
-    );
-    
-    assign mem_data = address ; 
-
-endmodule
--- rtl/mips_cpu_pc.v
+++
@@ -1,9 +0,0 @@
-module pc(
-    input logic[31:0] pc_in,
-    input logic pc_write_cond,
-    input logic pc_write,
-    input logic alu_zero,
-    output logic[31:0] pc_out
-);
-    
-endmodule
