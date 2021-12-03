80785eadbdd5cd9e8df903fb3097ef82ebdec6db
Dominic Justice Konec <dom.justicekonec@gmail.com>
2021-12-03 20:29:05 +0000

    implemented 3 state cycles and byte enable + instr

--- rtl/mips_cpu_controller.v
+++ rtl/mips_cpu_controller.v
@@ -11,12 +11,15 @@ module controler (
     output logic [1:0] pcsource,
     output logic pcwritecond,
     output logic jump,
+    output logic threecycle,
     output logic memread,
     output logic memwrite,
+    output logic [3:0] byteenable,
     output logic [1:0] memtoreg,
     output logic [3:0] aluop,
+    output logic muldivwrite,
     output logic alusrca,
-    output logic [1:0] alusrcb
+    output logic [2:0] alusrcb
 
 );
 
@@ -26,7 +29,7 @@ module controler (
     exec1 = (state == 3);
     exec2 = (state == 4);
 
-    if (state == 1) begin
+    if (state == 1) begin  //fetch
       regdst = 0;
       regwrite = 0;
       iord = 0;
@@ -35,14 +38,16 @@ module controler (
       pcsource = 0;
       pcwritecond = 0;
       jump = 0;
+      threecycle = 0;
       memread = 1;
       memwrite = 0;
+      byteenable = 0;
       memtoreg = 0;
       aluop = 0;
       alusrca = 0;
       alusrcb = 1;
 
-    end else if (state == 2) begin
+    end else if (state == 2) begin  //decode
       regdst = 0;
       regwrite = 0;
       iord = 0;
@@ -51,27 +56,31 @@ module controler (
       pcsource = 0;
       pcwritecond = 0;
       jump = 0;
+      threecycle = 0;
       memread = 0;
       memwrite = 0;
+      byteenable = 0;
       memtoreg = 0;
       aluop = 0;
       alusrca = 0;
       alusrcb = 3;
 
-    end else begin
+    end else begin  //exec1 and exec2
       case (opcode)
         6'h0: begin  //Rtype
-          if (fncode >= 5'h20 && fncode <= 5'h26) begin  //Arithmetic and logical
+          if (fncode  == 5'h4  || fncode == 5'h6 || fncode == 5'h7 || fncode == 5'h21 || fncode == 5'h23 || fncode == 5'h24 || fncode == 5'h25 || fncode == 5'h26 || fncode == 5'h2a || fncode == 5'h2b) begin  //Arithmetic and logical
             regdst = 1;
-            regwrite = exec2;
+            regwrite = 1;
             iord = 0;
             irwrite = 0;
             pcwrite = 0;
             pcsource = 0;
             pcwritecond = 0;
             jump = 0;
+            threecycle = 1;
             memread = 0;
             memwrite = 0;
+            byteenable = 0;
             memtoreg = 0;
             aluop = 2;
             alusrca = 1;
@@ -84,63 +93,71 @@ module controler (
             pcwrite = 0;
             pcsource = 3;
             pcwritecond = 0;
-            jump = exec2;
+            jump = 1;
+            threecycle = 1;
             memread = 0;
             memwrite = 0;
+            byteenable = 0;
             memtoreg = 0;
             aluop = 0;
             alusrca = 0;
             alusrcb = 0;
           end else if (fncode == 5'h9) begin  //JALR
             regdst = 1;
-            regwrite = exec1;
+            regwrite = 1;
             iord = 0;
             irwrite = 0;
             pcwrite = 0;
             pcsource = 3;
             pcwritecond = 0;
-            jump = exec2;
+            jump = 1;
+            threecycle = 1;
             memread = 0;
             memwrite = 0;
+            byteenable = 0;
             memtoreg = 0;
             aluop = 0;
             alusrca = 0;
             alusrcb = 1;
           end
         end
-        6'h23: begin  //lw
-          regdst = 0;
-          regwrite = exec2;
-          iord = 1;
-          irwrite = 0;
-          pcwrite = 0;
-          pcsource = 0;
-          pcwritecond = 0;
-          jump = 0;
-          memread = exec1;
-          memwrite = 0;
-          memtoreg = 1;
-          aluop = 0;
-          alusrca = 1;
-          alusrcb = 2;
-        end
-        6'h2b: begin  //sw
-          regdst = 0;
+        6'h2: begin  //J
           regwrite = 0;
-          iord = 1;
+          regdst = 0;
+          iord = 0;
           irwrite = 0;
           pcwrite = 0;
-          pcsource = 0;
+          pcsource = 2;
           pcwritecond = 0;
-          jump = 0;
+          jump = 1;
+          threecycle = 1;
           memread = 0;
-          memwrite = exec2;
+          memwrite = 0;
+          byteenable = 0;
           memtoreg = 0;
           aluop = 0;
-          alusrca = 1;
-          aluscr2 = 2;
+          alusrca = 0;
+          aluscrb = 0;
         end
-        6'h4: begin  //beq
+        6'h3: begin  //JAL
+          regdst = 1;
+          regwrite = 1;
+          iord = 0;
+          irwrite = 0;
+          pcwrite = 0;
+          pcsource = 2;
+          pcwritecond = 0;
+          jump = 1;
+          threecycle = 1;
+          memread = 0;
+          memwrite = 0;
+          byteenable = 0;
+          memtoreg = 0;
+          aluop = 0;
+          alusrca = 0;
+          alusrcb = 1;
+        end
+        6'h4: begin  //BEQ
           regwrite = 0;
           regdst = 0;
           iord = 0;
@@ -149,13 +166,88 @@ module controler (
           pcsource = 1;
           pcwritecond = 1;
           jump = 0;
+          threecycle = 1;
           memread = 0;
           memwrite = 0;
+          byteenable = 0;
           memtoreg = 0;
-          aluop = 1;
+          aluop = 3;
           alusrca = 1;
           aluscrb = 0;
         end
+        6'h8: begin  //ADDIU
+          regdst = 0;
+          regwrite = 1;
+          iord = 0;
+          irwrite = 0;
+          pcwrite = 0;
+          pcsource = 0;
+          pcwritecond = 0;
+          jump = 0;
+          threecycle = 1;
+          memread = 0;
+          memwrite = 0;
+          byteenable = 0;
+          memtoreg = 0;
+          aluop = 0;
+          alusrca = 1;
+          alusrcb = 2;
+        end
+        6'hc || 6'hd || 6'he: begin  //ANDI, ORI, XORI
+          regdst = 0;
+          regwrite = 1;
+          iord = 0;
+          irwrite = 0;
+          pcwrite = 0;
+          pcsource = 0;
+          pcwritecond = 0;
+          jump = 0;
+          threecycle = 1;
+          memread = 0;
+          memwrite = 0;
+          byteenable = 0;
+          memtoreg = 0;
+          aluop = opcode == 6'hc ? 0 : opcode == 6'hd ? 0 : 0; //TODO: correct when i instructions are in alu control
+          alusrca = 1;
+          alusrcb = 4;
+        end
+        6'h23: begin  //LW
+          regdst = 0;
+          regwrite = exec2;
+          iord = 1;
+          irwrite = 0;
+          pcwrite = 0;
+          pcsource = 0;
+          pcwritecond = 0;
+          jump = 0;
+          threecycle = 0;
+          memread = exec1;
+          memwrite = 0;
+          byteenable = 0;
+          memtoreg = 1;
+          aluop = 0;
+          alusrca = 1;
+          alusrcb = 2;
+        end
+        6'h2b: begin  //SW
+          regdst = 0;
+          regwrite = 0;
+          iord = 1;
+          irwrite = 0;
+          pcwrite = 0;
+          pcsource = 0;
+          pcwritecond = 0;
+          jump = 0;
+          threecycle = 1;
+          memread = 0;
+          memwrite = 1;
+          byteenable = 4'b1111;
+          memtoreg = 0;
+          aluop = 0;
+          alusrca = 1;
+          aluscr2 = 2;
+        end
+
 
         default: begin
           $fatal(1, "opcode not yet implemented");
