ee9279699a657b0993ad4ce5dc1b6bd316b82518
Dominic Justice Konec <dom.justicekonec@gmail.com>
2021-12-03 17:00:33 +0000

    formatting and small typo

--- rtl/mips_cpu_alu.v
+++ rtl/mips_cpu_alu.v
@@ -1,42 +1,43 @@
-module ALU(
-  //input logic[5:0] opcode,
-  input logic[4:0] alu_func,
-  input logic[31:0] a,
-  input logic[31:0] b,
-  input logic[4:0] shift,
-  output logic[31:0] result,
-  output logic zero
+module ALU (
+    //input logic[5:0] opcode,
+    input logic [4:0] alu_func,
+    input logic [31:0] a,
+    input logic [31:0] b,
+    input logic [4:0] shift,
+    output logic [31:0] result,
+    output logic zero
 );
 
 
-always_comb begin 
-  case (alu_func)
-    5'b00000 : result = $unsigned(a) + $unsigned(b); // ADDU  (immediate or not determined elsewhere)
-    5'b00001 : result = a & b; // AND
-    5'b00010 : result = a | b; // OR
-    5'b00011 : result = $unsigned(a) - $unsigned(b); // SUBU
-    5'b00100 : result = $signed(a) < $signed(b); // SLT
-    5'b00101 : result = $unsigned(a) < $unsigned(b); // SLTU
-    5'b00110 : result = (a << shift); // SLL
-    5'b00111 : result = a << b; // SLLV
-    5'b01000 : result = a >> shift; // SRL
-    5'b01001 : result = a >> b; // SRLV
-    5'b01010 : result = $signed(a) >>> shift; // SRA
-    5'b01011 : result = $signed(a) >>> b; // SRAV
-    5'b01100 : result = a ^ b; // XOR
+  always_comb begin
+    case (alu_func)
+      5'b00000:
+      result = $unsigned(a) + $unsigned(b);  // ADDU  (immediate or not determined elsewhere)
+      5'b00001: result = a & b;  // AND
+      5'b00010: result = a | b;  // OR
+      5'b00011: result = $unsigned(a) - $unsigned(b);  // SUBU
+      5'b00100: result = $signed(a) < $signed(b);  // SLT
+      5'b00101: result = $unsigned(a) < $unsigned(b);  // SLTU
+      5'b00110: result = (a << shift);  // SLL
+      5'b00111: result = a << b;  // SLLV
+      5'b01000: result = a >> shift;  // SRL
+      5'b01001: result = a >> b;  // SRLV
+      5'b01010: result = $signed(a) >>> shift;  // SRA
+      5'b01011: result = $signed(a) >>> b;  // SRAV
+      5'b01100: result = a ^ b;  // XOR
 
-    //branch conditions
-    5'b01100 : result = (a == b); // BEQ
-    5'b01101 : result = $signed(a) >= 0; // BGEZ (and link?)
-    5'b01110 : result = $signed(a) > 0; // BGTZ
-    5'b01111 : result = $signed(a) <= 0; // BLEZ
-    5'b10000 : result = $signed(a) < 0; // BLTZ (and link?)
-    5'b10001 : result = (a != b); // BNE
-    default : result = 0;
-  endcase
-end
+      //branch conditions
+      5'b01100: result = (a == b);  // BEQ
+      5'b01101: result = $signed(a) >= 0;  // BGEZ (and link?)
+      5'b01110: result = $signed(a) > 0;  // BGTZ
+      5'b01111: result = $signed(a) <= 0;  // BLEZ
+      5'b10000: result = $signed(a) < 0;  // BLTZ (and link?)
+      5'b10001: result = (a != b);  // BNE
+      default:  result = 0;
+    endcase
+  end
 
 
-assign zero = (result == 0);
+  assign zero = (result == 0);
 
 endmodule
--- rtl/mips_cpu_mult_div.v
+++ rtl/mips_cpu_mult_div.v
@@ -1,27 +1,29 @@
-module mult(
-    input logic[31:0] a, b,
-    input logic[2:0] op,
+module mult (
+    input logic [31:0] a,
+    b,
+    input logic [2:0] op,
     input logic clk,
     input logic write,
     input logic reset,
 
-    output logic[31:0] hi, lo
+    output logic [31:0] hi,
+    lo
 );
 
-logic sign_a, sign_b, sign_out;
-logic[31:0] mag_a, mag_b, signed_a, signed_b, unsigned_a, unsigned_b;
-logic[63:0] div_u, mult_u, div, mult;
+  logic sign_a, sign_b, sign_out;
+  logic [31:0] mag_a, mag_b, signed_a, signed_b, unsigned_a, unsigned_b;
+  logic [63:0] div_u, mult_u, div, mult;
 
-assign sign_a = a[31];
-assign sign_b = b[31];
+  assign sign_a = a[31];
+  assign sign_b = b[31];
 
-always_comb begin 
+  always_comb begin
     signed_a = $signed(a);
     signed_b = $signed(b);
     unsigned_a = $unsigned(a);
     unsigned_b = $unsigned(b);
 
-    mag_a = sign_a ? -$signed(a) :  $unsigned(a);
+    mag_a = sign_a ? -$signed(a) : $unsigned(a);
     mag_b = sign_b ? -$signed(b) : $unsigned(b);
 
     div_u = unsigned_a / unsigned_b;
@@ -31,39 +33,40 @@ always_comb begin
 
     sign_out = sign_a + sign_b;
 
-end
+  end
 
-always @(posedge clk) begin
+  always @(posedge clk) begin
 
     if (reset) begin
-        hi <= 0;
-        lo <= 0;
+      hi <= 0;
+      lo <= 0;
     end else if (write) begin
-        case(op)
-            3'b000: begin
-                    hi <= div_u[63:32];
-                    lo <= div_u[31:0]; //DIVU
-                end 
-            3'b001: begin
-                    hi <= mult_u[63:32];
-                    lo <= mult_u[31:0]; //MULTU
-                end
-            3'b010: begin 
-                    hi[31] <= sign_out;
-                    hi[30:0] <= div[62:32];
-                    lo <= div[31:0];//DIV
-                end
-            3'b011: begin 
-                    hi[31] <= sign_out;
-                    hi[30:0] <= mult[62:32];
-                    lo <= mult[31:0];//MULT
-                end
-            3'b100: hi <= a; //MTHI
-            3'b101: lo <= a; //MTLO
-            3'b110: hi <= hi; //MFHI
-            3'b111: lo <= lo; //MFLO
-        endcase;
-    end 
-end
+      case (op)
+        3'b000: begin
+          hi <= div_u[63:32];
+          lo <= div_u[31:0];  //DIVU
+        end
+        3'b001: begin
+          hi <= mult_u[63:32];
+          lo <= mult_u[31:0];  //MULTU
+        end
+        3'b010: begin
+          hi[31] <= sign_out;
+          hi[30:0] <= div[62:32];
+          lo <= div[31:0];  //DIV
+        end
+        3'b011: begin
+          hi[31] <= sign_out;
+          hi[30:0] <= mult[62:32];
+          lo <= mult[31:0];  //MULT
+        end
+        3'b100: hi <= a;  //MTHI
+        3'b101: lo <= a;  //MTLO
+        3'b110: hi <= hi;  //MFHI
+        3'b111: lo <= lo;  //MFLO
+      endcase
+      ;
+    end
+  end
 
 endmodule
