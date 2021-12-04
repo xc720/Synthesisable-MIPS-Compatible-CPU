8372c7ffc74031abfcd6e429f8175267cef50923
Dominic Justice Konec <dom.justicekonec@gmail.com>
2021-12-04 19:30:34 +0000

    implemented more funtions

--- rtl/mips_cpu_controller.v
+++ rtl/mips_cpu_controller.v
@@ -49,6 +49,7 @@ module mips_cpu_controller (
       alusrca = 0;
       alusrcb = 1;
       aluouten = 1;
+      muldivwrite = 0;
 
     end else if (state == 2) begin  //decode
       regdst = 0;
@@ -68,65 +69,113 @@ module mips_cpu_controller (
       alusrca = 0;
       alusrcb = 3;
       aluouten = 1;
+      muldivwrite = 0;
 
     end else begin  //exec1 and exec2
       case (opcode)
-        6'h0: begin  //Rtype
-          if (fncode  == 6'h4  || fncode == 6'h6 || fncode == 6'h7 || fncode == 6'h21 || fncode == 6'h23 || fncode == 6'h24 || fncode == 6'h25 || fncode == 6'h26 || fncode == 6'h2a || fncode == 6'h2b) begin  //Arithmetic and logical
-            regdst = 1;
-            regwrite = 1;
-            iord = 0;
-            irwrite = 0;
-            pcwrite = 0;
-            pcsource = 0;
-            pcwritecond = 0;
-            jump = 0;
-            threecycle = 1;
-            memread = 0;
-            memwrite = 0;
-            byteenable = 0;
-            memtoreg = 0;
-            aluop = 2;
-            alusrca = 1;
-            alusrcb = 0;
-            aluouten = 1;
-          end else if (fncode == 6'h8) begin  //JR
-            regdst = 0;
-            regwrite = 0;
-            iord = 0;
-            irwrite = 0;
-            pcwrite = 0;
-            pcsource = 3;
-            pcwritecond = 0;
-            jump = 1;
-            threecycle = 1;
-            memread = 0;
-            memwrite = 0;
-            byteenable = 0;
-            memtoreg = 0;
-            aluop = 0;
-            alusrca = 0;
-            alusrcb = 0;
-            aluouten = 1;
-          end else if (fncode == 6'h9) begin  //JALR
-            regdst = 1;
-            regwrite = 1;
-            iord = 0;
-            irwrite = 0;
-            pcwrite = 0;
-            pcsource = 3;
-            pcwritecond = 0;
-            jump = 1;
-            threecycle = 1;
-            memread = 0;
-            memwrite = 0;
-            byteenable = 0;
-            memtoreg = 0;
-            aluop = 0;
-            alusrca = 0;
-            alusrcb = 1;
-            aluouten = 1;
-          end
+        6'h0: begin  //Rtype and Special
+          case (fncode)
+            6'h0 || 6'h2 || 6'h3 || 6'h4 || 6'h6 || 6'h7 || 6'h21 || 6'h23 || 6'h24 || 6'h25 || 6'h26 || 6'h2a || 6'h2b: begin  //SLL, SRL, SRA, SLLV, SRLV, SRAV, ADDU, SUBU, AND, OR, XOR, SLT, SLTU
+              regdst = 1;
+              regwrite = 1;
+              iord = 0;
+              irwrite = 0;
+              pcwrite = 0;
+              pcsource = 0;
+              pcwritecond = 0;
+              jump = 0;
+              threecycle = 1;
+              memread = 0;
+              memwrite = 0;
+              byteenable = 0;
+              memtoreg = 0;
+              aluop = 2;
+              alusrca = 1;
+              alusrcb = 0;
+              aluouten = 1;
+              muldivwrite = 0;
+            end
+            6'h8: begin  //JR
+              regdst = 0;
+              regwrite = 0;
+              iord = 0;
+              irwrite = 0;
+              pcwrite = 0;
+              pcsource = 3;
+              pcwritecond = 0;
+              jump = 1;
+              threecycle = 1;
+              memread = 0;
+              memwrite = 0;
+              byteenable = 0;
+              memtoreg = 0;
+              aluop = 0;
+              alusrca = 0;
+              alusrcb = 0;
+              aluouten = 1;
+              muldivwrite = 0;
+            end
+            6'h9: begin  //JALR
+              regdst = 1;
+              regwrite = 1;
+              iord = 0;
+              irwrite = 0;
+              pcwrite = 0;
+              pcsource = 3;
+              pcwritecond = 0;
+              jump = 1;
+              threecycle = 1;
+              memread = 0;
+              memwrite = 0;
+              byteenable = 0;
+              memtoreg = 0;
+              aluop = 0;
+              alusrca = 0;
+              alusrcb = 1;
+              aluouten = 1;
+              muldivwrite = 0;
+            end
+            6'h10 || 6'h12: begin  //MFHI, MFLO
+              regdst = 1;
+              regwrite = 1;
+              iord = 0;
+              irwrite = 0;
+              pcwrite = 0;
+              pcsource = 0;
+              pcwritecond = 0;
+              jump = 0;
+              threecycle = 1;
+              memread = 0;
+              memwrite = 0;
+              byteenable = 0;
+              memtoreg = 0;
+              aluop = 9;
+              alusrca = 0;
+              alusrcb = 0;
+              aluouten = 1;
+              muldivwrite = 0;
+            end
+            6'h11 || 6'h13 || 6'h18 || 6'h19 || 6'h1a || 6'h1b: begin //MTHI, MTLO, MULT, MULTU, DIV, DIVU
+              regdst = 0;
+              regwrite = 0;
+              iord = 0;
+              irwrite = 0;
+              pcwrite = 0;
+              pcsource = 0;
+              pcwritecond = 0;
+              jump = 0;
+              threecycle = 1;
+              memread = 0;
+              memwrite = 0;
+              byteenable = 0;
+              memtoreg = 0;
+              aluop = 9;
+              alusrca = 1;
+              alusrcb = 0;
+              aluouten = 1;
+              muldivwrite = 1;
+            end
+          endcase
         end
         6'h1: begin  //REGIMM     BLTZ, BLTZAL, BGEZ, BGEZAL
           if (regimm < 2) begin  //BLTZ, BGEZ
@@ -147,6 +196,7 @@ module mips_cpu_controller (
             alusrca = 1;
             alusrcb = 0;
             aluouten = 1;
+            muldivwrite = 0;
           end else begin  //BLTZAL, BGEZAL
             regdst = 2;
             regwrite = exec1;
@@ -165,6 +215,7 @@ module mips_cpu_controller (
             alusrca = exec2;
             alusrcb = 1;
             aluouten = 0;
+            muldivwrite = 0;
           end
         end
         6'h2: begin  //J
@@ -185,6 +236,7 @@ module mips_cpu_controller (
           alusrca = 0;
           alusrcb = 0;
           aluouten = 1;
+          muldivwrite = 0;
         end
         6'h3: begin  //JAL
           regdst = 1;
@@ -204,6 +256,7 @@ module mips_cpu_controller (
           alusrca = 0;
           alusrcb = 1;
           aluouten = 1;
+          muldivwrite = 0;
         end
         6'h4 || 6'h5 || 6'h6 || 6'h7: begin  //BEQ, BNE, BLEZ, BGTZ
           regwrite = 0;
@@ -223,6 +276,7 @@ module mips_cpu_controller (
           alusrca = 1;
           alusrcb = 0;
           aluouten = 1;
+          muldivwrite = 0;
         end
         6'h8: begin  //ADDIU
           regdst = 0;
@@ -242,8 +296,9 @@ module mips_cpu_controller (
           alusrca = 1;
           alusrcb = 2;
           aluouten = 1;
+          muldivwrite = 0;
         end
-        6'hc || 6'hd || 6'he: begin  //ANDI, ORI, XORI
+        6'ha || 6'hb || 6'hc || 6'hd || 6'he: begin  //SLTI. SLTIU ANDI, ORI, XORI
           regdst = 0;
           regwrite = 1;
           iord = 0;
@@ -257,10 +312,11 @@ module mips_cpu_controller (
           memwrite = 0;
           byteenable = 0;
           memtoreg = 0;
-          aluop = opcode == 6'hc ? 10 : opcode == 6'hd ? 11 : 12;
+          aluop = opcode == 6'ha ? 13 : opcode == 6'hb ? 14 : opcode == 6'hc ? 10 : opcode == 6'hd ? 11 : 12; //TODO: fix aluOP for SLTs when in alucontrol
           alusrca = 1;
           alusrcb = 4;
           aluouten = 1;
+          muldivwrite = 0;
         end
         6'h23: begin  //LW
           regdst = 0;
@@ -280,6 +336,7 @@ module mips_cpu_controller (
           alusrca = 1;
           alusrcb = 2;
           aluouten = 1;
+          muldivwrite = 0;
         end
         6'h28 || 6'h29 || 6'h2b: begin  //SB, SH, SW //TODO: implement SB, SH
           regdst = 0;
@@ -299,11 +356,9 @@ module mips_cpu_controller (
           alusrca = 1;
           alusrcb = 2;
           aluouten = 1;
+          muldivwrite = 0;
         end
-
       endcase
     end
   end
-
-
 endmodule
