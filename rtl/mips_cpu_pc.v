4ec510f40ee806400aa4d6dd1456eb09843277d4
Andyli-1020 <andyli1020@outlook.com>
2021-12-05 19:17:05 +0000

    update

--- rtl/mips_cpu_pc.v
+++ rtl/mips_cpu_pc.v
@@ -1,9 +1,18 @@
 module mips_cpu_pc (
     input logic clk,
     input logic jmp,
+    input logic jr,
     input logic reset,
+    input logic beq_result,
+    /*branch if assert*/
+    input logic bgez_result,
+    /*branch if assert*/
+    input logic bgezal_result,
+    input logic bgtz_result,
+    input logic blez_result,
+    input logic bltz_result,
+    input logic bltzal_result,
     input logic [31:0] pcin,
-
     output logic [31:0] pcout
 );
 
