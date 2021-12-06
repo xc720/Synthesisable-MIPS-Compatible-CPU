22b24202a0da2d68914d00c38c98b8f6de7a5242
Dominic Justice Konec <dom.justicekonec@gmail.com>
2021-12-06 12:46:49 +0000

    rename and format

--- rtl/pc.v
+++ rtl/pc.v
@@ -1,22 +1,21 @@
-module pc(
-  input logic clk,
-  input logic jmp,
-  input logic reset,
-  input logic[31:0] pcin,
+module pc (
+    input logic clk,
+    input logic jmp,
+    input logic reset,
+    input logic [31:0] pcin,
 
-  output logic[31:0] pcout
+    output logic [31:0] pcout
 );
 
 
-always_ff @(posedge clk) begin
+  always_ff @(posedge clk) begin
 
-  if(reset) begin
-    pcout <= 32'hBFC00000;
-  end
-  else if(jmp) begin
-    pcout <= pcin;
-  end
+    if (reset) begin
+      pcout <= 32'hBFC00000;
+    end else if (jmp) begin
+      pcout <= pcin;
+    end
 
-end
+  end
 
 endmodule
