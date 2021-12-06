ef42f125139565f410731b79a1043e0a8b8ccf42
Gian-Luca Fenocchi <lucabod8@gmail.com>
2021-12-06 21:46:18 +0000

    changes

--- test/asm_test_cases/lwr2.asm
+++ test/asm_test_cases/lwr2.asm
@@ -1,6 +1,6 @@
-LUI $a0, 1024
-ADDIU $a0, $a0, 128
-ADDIU $a1, $a1, 256
-SW $a0, 0($a1)
-LWR $v0, 2($a1)
-JR $zero
+LUI $a0, 1024           #
+ADDIU $a0, $a0, 128     #
+ADDIU $a1, $a1, 256     #
+SW $a0, 0($a1)          #
+LWR $v0, 2($a1)         #
+JR $zero                #
