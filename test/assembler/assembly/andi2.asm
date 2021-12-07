81d107df827c5b274b9b9bec2469d68cd07be94e
xc720 <xc720@ic.ac.uk>
2021-12-07 22:58:56 +0000

    fix

--- test/assembler/assembly/andi2.asm
+++ test/assembler/assembly/andi2.asm
@@ -1,3 +1,5 @@
 ADDIU $a0, $a0, 1024                              #
-ANDI $v0, $a0, 1024                               #
-JR $zero      #immediate 1024 || $a0(1024)      #
+LUI $a0, 24246
+ADDIU $a0, $a0, 12346
+ANDI $v0, $a0, 12123
+JR $zero
