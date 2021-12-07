9c14067f90e1d5c1719eedf5643bcfd3d81543bb
Andyli-1020 <andyli1020@outlook.com>
2021-12-07 16:43:48 +0000

    add new assembly

---
+++ test/asm_test_cases/lb1.asm
@@ -0,0 +1,9 @@
+ADDIU $a1, $a1, 4095   # a1 = 0xFFF
+ADDIU $a1, $a1, 4095   # a1 = 0x1FFE
+ADDIU $a1, $a1, 4095   # a1 = 0x2FFD
+ADDIU $a1, $a1, 4095   # a1 = 0x3FFC
+ADDIU $a1, $a1, 4095   # a1 = 0x4FFB
+ADDIU $a2, $a2, 200    # a2 = 200
+SW $a1, 0($a2)         # mem(200) = 0x00004FFB
+LB $v0, 1($a2)         # v0 = mem(201) = 0x00
+JR $zero               #
