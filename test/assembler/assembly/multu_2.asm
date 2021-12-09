f59844c0dfcf58f122f85de1849df5d21053810c
xc720 <xc720@ic.ac.uk>
2021-12-09 16:29:45 +0000

    rewrite mtgi mult and multu

--- test/assembler/assembly/multu_2.asm
+++ test/assembler/assembly/multu_2.asm
@@ -1,5 +1,5 @@
-ADDIU $a0, $a0, 64
-SUBU $a1, $a1, $a0
-MULTU $a0, $a1
-MFLO $v0
-JR $zero
+ADDIU $a0, $a0, 999999         #        
+ADDIU $a1, $a1, 99999        #
+MULTU $a0, $a1               #
+MFHI $v0                    #
+JR $zero                    #v0=23
--- test/assembler/assembly/multu_3.asm
+++ test/assembler/assembly/multu_3.asm
@@ -1,4 +1,5 @@
-ADDIU $a0, $a0, 64
-MULTU $a0, $zero
-MFLO $v0
-JR $zero
+ADDIU $a0, $a0, 99999          #
+SUBU $a1, $a1, $a0          #
+MULTU $a0, $a1               #
+MFHI $v0                    #
+JR $zero                    #v0=99996 instead of -3
