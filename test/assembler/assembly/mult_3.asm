3dae8cad90ab96981c74f3bf31bf2e8e98793be7
xc720 <xc720@ic.ac.uk>
2021-12-09 17:25:34 +0000

    operand in correct range

--- test/assembler/assembly/mult_3.asm
+++ test/assembler/assembly/mult_3.asm
@@ -1,5 +1,5 @@
-ADDIU $a0, $a0, 99999          #
+ADDIU $a0, $a0, 32767          #
 SUBU $a1, $a1, $a0          #
 MULT $a0, $a1               #
 MFHI $v0                    #
-JR $zero                    #v0=-3
+JR $zero                    #v0=-1
--- test/assembler/assembly/multu_3.asm
+++ test/assembler/assembly/multu_3.asm
@@ -1,5 +1,5 @@
-ADDIU $a0, $a0, 99999          #
+ADDIU $a0, $a0, 32767          #
 SUBU $a1, $a1, $a0          #
 MULTU $a0, $a1               #
 MFHI $v0                    #
-JR $zero                    #v0=99996 instead of -3
+JR $zero                    #v0=32766
