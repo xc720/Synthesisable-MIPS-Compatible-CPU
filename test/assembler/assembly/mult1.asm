b3a8609cb4b9ca4904aaff3c22a1bdac5904886a
xc720 <xc720@ic.ac.uk>
2021-12-09 17:25:34 +0000

    operand in correct range

--- test/assembler/assembly/mult1.asm
+++ test/assembler/assembly/mult1.asm
@@ -1,5 +1,7 @@
-ADDIU $a0, $a0, 999999         #        
-ADDIU $a1, $a1, 99999        #
+ADDIU $a0, $a0, 32767         #        
+ADDIU $a1, $a1, 32767        #
 MULT $a0, $a1               #
+MFLO $a3                    #
+MULT $a3, $a1               #
 MFLO $v0                    #
-JR $zero                    #v0=1214652193
+JR $zero                    #v0=1073840127
