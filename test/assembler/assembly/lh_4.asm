83e527eb5355417bbf1f738c639f2935b00f20d1
sheriff4000 <sheriff4000@gmail.com>
2021-12-12 14:33:55 +0000

    more test results

--- test/assembler/assembly/lh_4.asm
+++ test/assembler/assembly/lh_4.asm
@@ -7,5 +7,5 @@ ADDIU $a2, $a2, 0      # a2 = 0x00000
 SUBU  $a2, $a2, $a0    # a2 = 0x0B005/-20475
 ADDIU $a1, $a1, 200    # a1 = 200
 SW $a2, 0($a1)         # mem(200) = 0xFFFFB005/-20475
-LH $v0, 3($a1)         # v0 = mem(203) = 0xB005
+LH $v0, 2($a1)         # v0 = mem(202) = 0xffff = -1
 JR $zero                 #
--- test/assembler/assembly/lwl_2.asm
+++ test/assembler/assembly/lwl_2.asm
@@ -1,6 +1,6 @@
-LUI $a0, 1024           #            
-ADDIU $a0, $a0, 128     #
-ADDIU $a1, $a1, 256     #
-SW $a0, 0($a1)          #
-LWL $v0, 2($a1)         #
+LUI $a0, 1024           #a0 = 0x400           
+ADDIU $a0, $a0, 128     #a0 = 0x480
+ADDIU $a1, $a1, 256     #a1 = 0x100
+SW $a0, 0($a1)          #mem(256) = 0x00000480
+LWL $v0, 2($a1)         #v0 = 0x0000
 JR $zero                #
