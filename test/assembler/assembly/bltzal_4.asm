3aa77be5165f629daa0648811bd6940f86a3b229
xc720 <xc720@ic.ac.uk>
2021-12-14 17:39:03 +0000

    fix results

--- test/assembler/assembly/bltzal_4.asm
+++ test/assembler/assembly/bltzal_4.asm
@@ -1,8 +1,9 @@
-ADDIU $a0, $a0, 0
-BLTZAL $a0, SmallerZero
+ADDIU $a0, $a0, 1
+SUBU $a1, $a1, $a0
+BLTZAL $a1, SmallerZero
 NOP
 ADDIU $v0, $v0, 60
 ADDIU $v0, $v0, 100
 SmallerZero:
-ADDIU $v0, $v0, 3
-JR $zero
+ADDU $v0, $v0, $ra
+JR $zero            #v0=4194316
