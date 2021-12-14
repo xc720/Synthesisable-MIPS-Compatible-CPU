6168c2447d3607aa9617307385f0adaf71529d7d
sheriff4000 <sheriff4000@gmail.com>
2021-12-14 18:16:33 +0000

    srav and srlv

--- test/assembler/assembly/srlv_4.asm
+++ test/assembler/assembly/srlv_4.asm
@@ -1,4 +1,4 @@
-ADDIU $a0, $a0, 2
-ADDIU $a1, $a1, 18
-SRLV $v0, $a0, $a1
+ADDIU $a0, $a0, 18 #a0 = 0x12 = 0b00010010
+ADDIU $a1, $a1, 2 #a1 = 0x2 = 0b10
+SRLV $v0, $a0, $a1 #v0 = 0b100 = 4
 JR $zero
