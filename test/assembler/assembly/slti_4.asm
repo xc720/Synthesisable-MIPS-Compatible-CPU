02fd8d8c3cb4bd9b5b14caab33209b87b213eef5
sheriff4000 <sheriff4000@gmail.com>
2021-12-16 10:53:02 +0000

    slti

--- test/assembler/assembly/slti_4.asm
+++ test/assembler/assembly/slti_4.asm
@@ -1,3 +1,4 @@
-ADDIU $a0, $a0, 1       # a0 = 1
-SLTI $v0, $a0, 65535    # v0 = 1 <= -1 = 0
+ADDIU $a0, $zero, 1     # a0 = 1
+SLTI $v0, $a0, -1   # v0 = 1 < -1 = 0
+xori $v0, $a0, 0xffff
 JR $zero
