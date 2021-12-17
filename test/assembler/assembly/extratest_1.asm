8f81bef4292a1dd1cab5f150796d9787e9c53542
Dominic Justice Konec <dom.justicekonec@gmail.com>
2021-12-17 20:24:08 +0000

    extratest

--- test/assembler/assembly/extratest_1.asm
+++ test/assembler/assembly/extratest_1.asm
@@ -1,5 +1,5 @@
 ADDIU $a0, $0, 24       #a0=24
-ADDIU $a1, $0, 3        #a1=3
+ADDIU $a1, $0, 0x100        #a1=256
 BGEZ $a0, greaterthanzero    #a0>0 this will be executed
 ADDIU $a3, $0, 3
 greaterthanzero:        #skipped
@@ -10,6 +10,6 @@ SLLV $v0, $a0, $t1      #v0=24*(2^3)=24*8=192
 SLTIU $t2, $v0, 466     #t2=1
 MULT $v0, $v0           #192*192=36864
 MFLO $v0                #v0=36864=0x00009000
-SW $v0, 0($a1)          #mem(3)=0x00900000
-LWR $v0, 0($a1)         #
+SW $v0, 0($a1)          #mem(256)=0x00900000
+LWR $v0, 2($a1)         #v0=0x00000090
 JR  $zero               #
