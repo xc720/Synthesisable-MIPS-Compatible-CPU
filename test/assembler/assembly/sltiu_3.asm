b2012e7276040a982fef0c814a33a5377069e41d
xc720 <xc720@ic.ac.uk>
2021-12-17 19:54:53 +0000

    new test

--- test/assembler/assembly/sltiu_3.asm
+++ test/assembler/assembly/sltiu_3.asm
@@ -1,6 +1,5 @@
 ADDIU $a0, $a0, 3
-SUBU $v0, $a1, $a0
-SLTIU $v0, $v0, 3
-xori $v0, $v0, 65535			# $v0 = $t1 ^ 0xffff = 
-
+SUBU $v0, $a1, $a0      #v0=-3-->unsigned=4294967293
+SLTIU $v0, $v0, 3       #v0=0
+xori $v0, $v0, 65535    #v0=65535
 JR $zero
